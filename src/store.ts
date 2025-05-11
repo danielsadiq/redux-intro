61e876b4fb68af2f14d85989cc3f74023f710667
Daniel Sadiq <danielsadiq93@gmail.com>
2025-05-11 17:27:23 +0100

    account reducer add

---
+++ src/store.ts
@@ -0,0 +1,32 @@
+interface StateType {
+  balance: number;
+  loan: number;
+  loanPurpose: string;
+}
+
+const initState: StateType = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
